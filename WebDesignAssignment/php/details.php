7d6d5c0ece53b55a9f2aca52681f55dba9a19f57
Patrick Thompson <32361791+patrickfrom98@users.noreply.github.com>
2018-12-30 00:30:29 +0000

    Update details.php

--- WebDesignAssignment/php/details.php
+++ WebDesignAssignment/php/details.php
@@ -23,7 +23,7 @@
 			$bookid = $_GET['id'];
 			
 			try {
-				$pdo = new PDO("mysql:host=localhost; dbname=u1756102", "u1756102", "08nov98");
+				$pdo = new PDO("mysql:host=localhost; dbname=", "", "");
 				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 				//Gets book info
